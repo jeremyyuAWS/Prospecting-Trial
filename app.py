e9c011eb549871b4953734bc84c05d1ae80e3b54
Praveen Saravanan <praveen@lyzr.ai>
2024-08-30 14:37:29 +0530

    added a recheck for empty filters
    Signed-off-by: Praveen Saravanan <praveen@lyzr.ai>

--- app.py
+++ app.py
@@ -16,28 +16,38 @@ buttonBool = st.button("Submit Query")
 
 if buttonBool and query:
     print("Step 1 - user query")
-    with st.spinner("Researching Target Audience...."):
+    with st.spinner("Scanning for Ideal Audience..."):
         target_audience = askLyzrAgent(query)
         print("Step 2 - target persona: " + target_audience)
-        st.markdown("✅Researched Target Audience")
+        st.markdown("🔍 Audience Insights Unlocked!")
     if target_audience:
-        with st.spinner("Researching on Apollo...."):
+        with st.spinner("Engineering Precision Filters..."):
             data_dict_for_apollo = getDict(target_audience)
             print("Step 3 - data dict for apollo: " + str(data_dict_for_apollo))
-            st.markdown("✅Researched Completed on Apollo")
+            st.markdown("🎯 Precision Filters Ready for Deployment!")
 
         data_dict_for_apollo_as_dict = json.loads(data_dict_for_apollo)
 
         writeToFile(data_dict_for_apollo, "input")
+
         if data_dict_for_apollo_as_dict:
-            with st.spinner("Asking to Apollo...."):
+            with st.spinner("Discovering Top Prospects..."):
                 data_in_json = searchPeople(data_dict_for_apollo_as_dict)
                 print("Step 4 - full response: " + str(data_in_json))
-                st.markdown("✅Asked to Apollo")
+                st.markdown("🚀 Top Prospects Ready for Action!")
 
             filtered_data = filterData(data_in_json)
             print("Filtered data: ")
             print(json.dumps(filtered_data, indent=4))
+
+            # Check if the filtered data is empty and re-run searchPeople if needed
+            if not filtered_data["people"]:
+                print("Filtered data is empty. Re-running searchPeople...")
+                with st.spinner("Reprocessing data..."):
+                    data_in_json = searchPeople(data_dict_for_apollo_as_dict)
+                    filtered_data = filterData(data_in_json)
+                    print("Reattempted Filtered data: ")
+                    print(json.dumps(filtered_data, indent=4))
 
             writeToFile(filtered_data, "output")
 
@@ -88,4 +98,4 @@ if buttonBool and query:
             </div>
         """, unsafe_allow_html=True)
 
-lyzr_demo_end(text="this app uses Lyzr Agents")
+lyzr_demo_end(text="Unlock the potential of AI-driven prospecting with our app, designed to seamlessly generate and refine high-value leads tailored to your business needs. This app was built using Lyzr Agents")
